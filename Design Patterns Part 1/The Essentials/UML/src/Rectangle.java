2d047626af76cca37a558f22dc10e5e3ab9ee71f
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2023-11-11 09:01:34 +0600

    Unified Modeling Language (UML)

---
+++ Design Patterns Part 1/The Essentials/UML/src/Rectangle.java
@@ -0,0 +1,9 @@
+public class Rectangle extends Shape {
+    private int width;
+    private int height;
+    private int positionY;
+
+    public void render() {
+        System.out.println("Render rectangle");
+    }
+}
