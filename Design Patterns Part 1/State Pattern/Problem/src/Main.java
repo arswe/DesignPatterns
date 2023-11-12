43972ae17921bab0575e85494ce16ee5f5b5b837
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2023-11-12 10:53:41 +0600

    Design Pattern

--- Design Patterns Part 1/State Pattern/Problem/src/Main.java
+++ Design Patterns Part 1/State Pattern/Problem/src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        var canvas = new Canvas();
+        canvas.setCurrentTool(new SelectionTool());
+        canvas.mouseDown();
+        canvas.mouseUp();
+
+        canvas.setCurrentTool(new BrushTool());
+        canvas.mouseDown();
+        canvas.mouseUp();
+
+        canvas.setCurrentTool(new EraserTool());
+        canvas.mouseDown();
+        canvas.mouseUp();
     }
 }
