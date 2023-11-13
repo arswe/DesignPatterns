4ac45184083fd91da8afacd4201e8c15520ad742
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2023-11-13 09:25:10 +0600

    Command Pattern

---
+++ Design Patterns Part 1/Command Pattern/Problem/src/Button.java
@@ -0,0 +1,16 @@
+public class Button {
+    private String label;
+
+    public void click() {
+        System.out.println("Click");
+    }
+
+
+    public String getLabel() {
+        return label;
+    }
+
+    public void setLabel(String label) {
+        this.label = label;
+    }
+}
