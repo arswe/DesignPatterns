7b4bbe7babdbada442c68b3720d10a5a023a5c0e
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2023-11-14 09:28:36 +0600

    Observer pattern Push Style

---
+++ Design Patterns Part 1/Observer Pattern/PushStyle/src/Chart.java
@@ -0,0 +1,6 @@
+public class Chart implements Observer {
+    @Override
+    public void update(int value) {
+        System.out.println("Chart got updated!"+ value);
+    }
+}
---
+++ Design Patterns Part 1/Observer Pattern/PushStyle/src/SpreadSheet.java
@@ -0,0 +1,6 @@
+public class SpreadSheet implements Observer {
+    @Override
+    public void update(int value) {
+        System.out.println("SpreadSheet got notified!" + value);
+    }
+}
