c79ccfc4ef62274b8075b6bba9ec71cd1380e2a3
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2023-11-14 09:15:50 +0600

    Observer pattern

---
+++ Design Patterns Part 1/Observer Pattern/Problem/src/DataSource.java
@@ -0,0 +1,11 @@
+public class DataSource {
+    private int value;
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+}
---
+++ Design Patterns Part 1/Observer Pattern/Problem/src/SpreadSheet.java
@@ -0,0 +1,2 @@
+public class SpreadSheet {
+}
