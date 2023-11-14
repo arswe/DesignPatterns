393951c50db648bd6929d28cf9ab706ec82a7d94
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2023-11-14 12:10:34 +0600

    Visitor Pattern

---
+++ Design Patterns Part 1/Visitor Pattern/Problem/src/HeadingNode.java
@@ -0,0 +1,6 @@
+public class HeadingNode implements HtmlNode {
+    @Override
+    public void highlight() {
+        System.out.println("Highlight-heading");
+    }
+}
---
+++ Design Patterns Part 1/Visitor Pattern/Problem/src/HtmlDocument.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class HtmlDocument {
+    private List<HtmlNode> nodes = new ArrayList<>();
+
+    public void add(HtmlNode node) {
+        nodes.add(node);
+    }
+
+    public void highlight() {
+        for (var node : nodes)
+            node.highlight();
+    }
+}
