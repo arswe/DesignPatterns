b098fd1780c546811c16f6bb9f253d05de4c52a1
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2023-11-14 09:51:02 +0600

    Mediator Pattern impliment

---
+++ Design Patterns Part 1/Mediator Pattern/Implementation/src/ArticlesDialogBox.java
@@ -0,0 +1,40 @@
+public class ArticlesDialogBox extends DialogBox {
+    private final ListBox articlesListBox = new ListBox(this);
+    private final TextBox titleTextBox = new TextBox(this);
+    private final Button saveButton = new Button(this);
+
+    public void simulateUserInteraction() {
+        articlesListBox.setSelection("Article 1");
+        titleTextBox.setContent("");
+        titleTextBox.setContent("Article 2");
+        System.out.println("TextBox: " + titleTextBox.getContent());
+        System.out.println("Button: " + saveButton.isEnabled());
+    }
+
+    @Override
+    public void changed(UIControl control) {
+        if (control == articlesListBox)
+            articleSelected();
+        else if (control == titleTextBox)
+            titleChanged();
+        else if (control == saveButton)
+            saveArticle();
+        else
+            throw new IllegalArgumentException();
+    }
+
+    private void saveArticle() {
+        // ...
+    }
+
+    private void titleChanged() {
+        var content = titleTextBox.getContent();
+        var isEmpty = (content == null || content.isEmpty());
+        saveButton.setEnabled(!isEmpty);
+    }
+
+    private void articleSelected() {
+        titleTextBox.setContent(articlesListBox.getSelection());
+        saveButton.setEnabled(true);
+    }
+}
