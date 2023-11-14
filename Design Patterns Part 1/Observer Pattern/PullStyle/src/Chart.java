d7df258154e03eaf2f235157167f0fe0a6763e3c
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2023-11-14 09:33:39 +0600

    Observer pattern Pull Style

---
+++ Design Patterns Part 1/Observer Pattern/PullStyle/src/Chart.java
@@ -0,0 +1,12 @@
+public class Chart implements Observer {
+    private final DataSource dataSource;
+
+    public Chart(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    @Override
+    public void update() {
+        System.out.println("Chart got updated!" + dataSource.getValue());
+    }
+}
---
+++ Design Patterns Part 1/Observer Pattern/PullStyle/src/SpreadSheet.java
@@ -0,0 +1,13 @@
+public class SpreadSheet implements Observer {
+    private final DataSource dataSource;
+
+    public SpreadSheet(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+
+    @Override
+    public void update() {
+        System.out.println("SpreadSheet got notified!" + dataSource.getValue());
+    }
+}
