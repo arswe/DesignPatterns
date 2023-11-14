94891cf128d2f5514d2161282a0a0031d3efdcd4
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2023-11-14 11:45:55 +0600

    Implementation Using the Observer Pattern

---
+++ Design Patterns Part 1/Mediator Pattern/UseObserverPattern/src/Button.java
@@ -0,0 +1,15 @@
+public class Button extends UIControl {
+    private boolean enabled;
+
+    public Button(DialogBox owner) {
+        super(owner);
+    }
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+}
