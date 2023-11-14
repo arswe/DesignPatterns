818bf9860f4a58600272b1a91eb764940fe9119c
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2023-11-14 09:38:47 +0600

    Mediator Pattern

---
+++ Design Patterns Part 1/Mediator Pattern/Problem/src/ListBox.java
@@ -0,0 +1,11 @@
+public class ListBox extends UIControl {
+    private String selection;
+
+    public String getSelection() {
+        return selection;
+    }
+
+    public void setSelection(String selection) {
+        this.selection = selection;
+    }
+}
