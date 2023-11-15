3b830ec6b7dafb6e1fc75879078a2328ae72368f
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2023-11-15 19:52:18 +0600

    Decorator Pattern Problem

---
+++ Design Patterns Part 2/Decorator Pattern/Problem/src/CompressedAndEncryptedCloudStream.java
@@ -0,0 +1,6 @@
+public class CompressedAndEncryptedCloudStream extends CloudStream {
+    @Override
+    public void write(String data) {
+        super.write(data);
+    }
+}
--- Design Patterns Part 2/Decorator Pattern/Problem/src/Main.java
+++ Design Patterns Part 2/Decorator Pattern/Problem/src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        var cloudStream = new EncryptedCloudStream();
+        var cloudStream = new CloudStream();
         cloudStream.write("Here is some data");
     }
 }
