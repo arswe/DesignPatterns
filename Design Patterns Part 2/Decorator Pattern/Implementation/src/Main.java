9c6131b1a111ea0b9342b7576fca99369ac422b4
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2023-11-15 20:00:29 +0600

    Decorator Pattern Problem Implementation

--- Design Patterns Part 2/Decorator Pattern/Implementation/src/Main.java
+++ Design Patterns Part 2/Decorator Pattern/Implementation/src/Main.java
@@ -1,6 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        storeCreditCard(new EncryptedCloudStream(new CompressedCloudStream(new CloudStream())));
+        var cloudStream = new CloudStream();
+        var encryptedCloudStream = new EncryptedCloudStream(cloudStream);
+        storeCreditCard(encryptedCloudStream);
     }
 
     public static void storeCreditCard(Stream stream) {
