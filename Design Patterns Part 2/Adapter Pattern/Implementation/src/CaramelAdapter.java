f3d9265f9a718cce5a38c15412207f14b61b4937
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2023-11-15 19:40:57 +0600

    Adapter Pattern Implementation

---
+++ Design Patterns Part 2/Adapter Pattern/Implementation/src/CaramelAdapter.java
@@ -0,0 +1,8 @@
+public class CaramelAdapter extends Caramel implements Filter {
+
+    @Override
+    public void apply(Image image) {
+        init();
+        render(image);
+    }
+}
--- Design Patterns Part 2/Adapter Pattern/Implementation/src/Main.java
+++ Design Patterns Part 2/Adapter Pattern/Implementation/src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         var imageView = new ImageView(new Image());
-        imageView.apply(new CaramelFilter(new Caramel()));
+        imageView.apply(new CaramelAdapter());
     }
 }
