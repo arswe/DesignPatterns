9a4ba09c4862cbeb523d6a67a99d0d577bc562cc
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2023-11-15 21:38:29 +0600

    Proxy Pattern  Implementation

---
+++ Design Patterns Part 2/Proxy Pattern/Implementation/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        var library = new Library();
+        String[] fileNames = {"a", "b", "c"};
+
+        for (var fileName : fileNames)
+            library.add(new LoggingEbookProxy(fileName));
+
+
+        library.openEbook("a");
+        library.openEbook("a");
+    }
+}
