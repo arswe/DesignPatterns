2f395d40455dac95b68a13dcba26276941b3f437
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2023-11-15 21:16:10 +0600

    Proxy Pattern

---
+++ Design Patterns Part 2/Proxy Pattern/Problem/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        var library = new Library();
+        String[] fileNames = {"a", "b", "c"};
+
+        for (var fileName : fileNames)
+            library.add(new Ebook(fileName));
+
+        library.openEbook("a");
+    }
+}
