ab22de70bf24a169ee98c0fe04c9e24164bb8029
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2024-01-19 10:32:20 +0600

    update

--- Design Patterns Part 3/Factory Method Pattern/Implementation/src/Main.java
+++ Design Patterns Part 3/Factory Method Pattern/Implementation/src/Main.java
@@ -2,6 +2,6 @@ public class Main {
     public static void main(String[] args) {
         var controller = new ProductsController();
         controller.listProducts();
-        System.out.println("Hello World!");
+
     }
 }
